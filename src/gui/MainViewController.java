bfd73df7e6fc6ec10e32a5c55f19e92433844cec
William Ramos <contato.williamramos@outlook.com>
2022-04-09 00:06:14 -0300

    DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 
@@ -60,6 +61,10 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 		}
 		catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,7 @@ module APP_Desktop {
 	
 	exports gui;
 	opens gui to javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
